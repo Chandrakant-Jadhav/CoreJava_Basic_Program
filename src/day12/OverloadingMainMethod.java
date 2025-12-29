eea869b7fc77b2df81f33252f70ac0e045dba048
Chandrakant <chandrakantjadhav.qa@gmail.com>
2025-12-29 15:22:13 +0530

    MainMethodOverloading

---
+++ src/day12/OverloadingMainMethod.java
@@ -0,0 +1,32 @@
+package day12;
+
+public class OverloadingMainMethod {
+	
+	
+	void main(int x)
+	{
+		System.out.println(x);
+	}
+	
+	
+	void main(String s)
+	{
+		System.out.println(s);
+	}
+	
+	void main(String s1, String s2)
+	{
+		System.out.println(s1+ s2);
+	}
+	
+
+	public static void main(String[] args) {
+		OverloadingMainMethod obj = new OverloadingMainMethod();
+		obj.main(100);
+		obj.main("Hello");
+		obj.main("Hello", "QA CJ");
+	
+
+	}
+
+}
