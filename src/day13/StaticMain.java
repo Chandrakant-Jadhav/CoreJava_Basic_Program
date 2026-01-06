d93ce14355d1a3f93bdf08dbba2a17d3efc6f27e
Chandrakant <chandrakantjadhav.qa@gmail.com>
2026-01-06 09:40:48 +0530

    Passing Params to Main Method

--- src/day13/StaticMain.java
+++ src/day13/StaticMain.java
@@ -1,5 +1,19 @@
 package day13;
 
 public class StaticMain {
+	public static void main(String[] args) {
+		//Accessing static members
+		//Static methods can directly accessing the static stuff (Without Creating Object) 
+		System.out.println(StaticDemo.a);
+		StaticDemo.m1();
+		
+		//Accessing non-static members
+		//static method can access non static stuff through object
+		
+		StaticDemo sd = new StaticDemo();
+		System.out.println(sd.b);
+		sd.m2();
+		
+	}
 
 }
